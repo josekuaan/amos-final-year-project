e6af46c4c8e9dfe67b7083b8ba0110be21516b2d
josekuaan <joshuaifeanyi67@gmail.com.com>
2022-06-10 14:01:03 -0700

    changed base url

--- client/src/base_url.js
+++ client/src/base_url.js
@@ -1,6 +1,6 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://amos-project.herokuapp.com";
 
 export default BASE_URL;
 
-//"proxy": "http://localhost:5000" http://localhost:3000 "https://amos-project.herokuapp.com"
+//"proxy": "http://localhost:5000" http://localhost:3000
 //$2b$10$q6oGf.sv85BR/Jhpq3WIcObZzBo2he60DUrcVguWMgUrnXogEY8qy
